5869e4abddde7497b092cd70a88a20392718f44a
MinhyeokSun <s.minyeok@gmail.com>
2021-12-22 23:30:29 +0900

    3rd Commit

--- app/src/main/java/com/smh/todaysquote/MainActivity.kt
+++ app/src/main/java/com/smh/todaysquote/MainActivity.kt
@@ -3,6 +3,11 @@ package com.smh.todaysquote
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,16 +20,52 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initViews()
+        initData()
     }
 
     private fun initViews() {
+
+    }
+
+    private fun initData() {
+        val remoteConfig = Firebase.remoteConfig
+        remoteConfig.setConfigSettingsAsync(
+            remoteConfigSettings {
+                minimumFetchIntervalInSeconds = 0
+            }
+        )
+
+        remoteConfig.fetchAndActivate().addOnCompleteListener {
+            if (it.isSuccessful) {
+                val quotes = parseQuotesJson(remoteConfig.getString("quotes"))
+                val isNameRevealed = remoteConfig.getBoolean("is_name_revealed")
+
+                displayQuotesPager(quotes, isNameRevealed)
+            }
+        }
+    }
+
+    private fun displayQuotesPager(quotes: List<Quote>, isNameRevealed: Boolean) {
         viewPager.adapter = QuotesPagerAdapter(
-            listOf(
-                Quote(
-                    "나는 생각한다. 고로 나는 존재한다.",
-                    "데카르트"
-                )
-            )
+            quotes = quotes,
+            isNameRevealed = isNameRevealed
         )
     }
+
+    private fun parseQuotesJson(json: String): List<Quote> {
+        val jsonArray = JSONArray(json)
+        var jsonList = emptyList<JSONObject>()
+        for (index in 0 until jsonArray.length()) {
+            val jsonObject = jsonArray.getJSONObject(index)
+            jsonObject?.let {
+                jsonList = jsonList + it
+            }
+        }
+
+        return jsonList.map {
+            Quote(it.getString("quote"), it.getString("name"))
+        }
+
+    }
+
 }
