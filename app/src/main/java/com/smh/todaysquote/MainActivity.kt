a686109ceaf53e9fdc8c6f12c3df33232365f573
MinhyeokSun <s.minyeok@gmail.com>
2021-12-23 13:43:14 +0900

    5th Commit

--- app/src/main/java/com/smh/todaysquote/MainActivity.kt
+++ app/src/main/java/com/smh/todaysquote/MainActivity.kt
@@ -2,17 +2,24 @@ package com.smh.todaysquote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
 import androidx.viewpager2.widget.ViewPager2
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import org.json.JSONArray
 import org.json.JSONObject
+import kotlin.math.absoluteValue
 
 class MainActivity : AppCompatActivity() {
 
     private val viewPager: ViewPager2 by lazy {
         findViewById(R.id.viewPager)
+    }
+
+    private val progressBar: ProgressBar by lazy {
+        findViewById(R.id.progressBar)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,8 +31,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
-
+        viewPager.setPageTransformer { page, position ->
+            when {
+                position.absoluteValue >= 1F -> {
+                    page.alpha = 0F
+                }
+                position == 0F -> {
+                    page.alpha = 1F
+                }
+                else -> {
+                    page.alpha = 1F - 2 * position.absoluteValue
+                }
+            }
+        }
     }
+
 
     private fun initData() {
         val remoteConfig = Firebase.remoteConfig
@@ -36,6 +56,7 @@ class MainActivity : AppCompatActivity() {
         )
 
         remoteConfig.fetchAndActivate().addOnCompleteListener {
+            progressBar.visibility = View.GONE
             if (it.isSuccessful) {
                 val quotes = parseQuotesJson(remoteConfig.getString("quotes"))
                 val isNameRevealed = remoteConfig.getBoolean("is_name_revealed")
